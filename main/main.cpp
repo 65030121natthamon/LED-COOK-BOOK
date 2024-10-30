3ecccbb08e34a7fcada5e66798d608d7ae88ddde
65030121natthamon <144195611+65030121natthamon@users.noreply.github.com>
2024-10-30 10:43:22 +0700

    editcode

--- main/main.cpp
+++ main/main.cpp
@@ -8,14 +8,27 @@ extern "C" void app_main(void);
 void app_main(void)
 {
     LED led1(5); 
-    LED led2(17); 
+    LED led2(17);
+    LED led3(18);  
+    
     while(1)
     {        
         led1.ON();
         led2.OFF();
-        vTaskDelay(500/portTICK_PERIOD_MS);
+        led3.OFF();
+        printf("LED 1 is ON\n");
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
         led1.OFF();
         led2.ON();
-        vTaskDelay(500/portTICK_PERIOD_MS);
+        led3.OFF();
+        printf("LED 2 is ON\n");
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+
+        led1.OFF();
+        led2.OFF();
+        led3.ON();
+        printf("LED 3 is ON\n");
+        vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
